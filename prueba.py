405a01e5f2779f7b26269771324272e5ae8d522c
Joygamarra09 <yoanagamarra09@gmail.com>
2025-03-29 11:55:07 -0300

    se agregó variable eestadoCivil

--- prueba.py
+++ prueba.py
@@ -2,5 +2,5 @@
 nombre = input("Por favor ingrese su nombre y apellido: ")
 edad = input("Por favor ingrese su edad: ")
 recidencia = input("Por favor ingrese su lugar de recidencia: ")
-
-print(f"Su nombre es {nombre}, su edad es {edad} años y su lugar de recidencia es {recidencia}. Sus datos han sido procesados con éxito.")
+estadoCivil = input("Por favor ingrese su stado civil: ")
+print(f"Su nombre es {nombre}, su edad es {edad} años, su lugar de recidencia es {recidencia} y su estado civil es {estadoCivil}. Sus datos han sido procesados con éxito.")
