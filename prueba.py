bccbb7455b9e89857ba2668d9facb37418da9dcb
Joygamarra09 <yoanagamarra09@gmail.com>
2025-03-29 11:50:50 -0300

    Primer commit

---
+++ prueba.py
@@ -0,0 +1,6 @@
+
+nombre = input("Por favor ingrese su nombre y apellido: ")
+edad = input("Por favor ingrese su edad: ")
+recidencia = input("Por favor ingrese su lugar de recidencia: ")
+
+print(f"Su nombre es {nombre}, su edad es {edad} años y su lugar de recidencia es {recidencia}. Sus datos han sido procesados con éxito.")
